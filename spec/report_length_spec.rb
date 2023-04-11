0b2bb123dd1d30941f4468763aa1402c633d190e
Sharmine <sharmine.ss@gmail.com>
2023-04-11 16:09:22 +0100

    added second test case to report_length

--- spec/report_length_spec.rb
+++ spec/report_length_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe "report_length method" do
         result = report_length("Hello World!")
         expect(result).to eq "This string was 12 characters long."
     end
+
+    it "returns how long the string entered is" do
+        result = report_length("Hi")
+        expect(result).to eq "This string was 2 characters long."
+    end
 end

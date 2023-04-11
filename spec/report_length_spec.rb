50a5e3e8a5e525007dab4d9a973890ba3e6baca1
Sharmine <sharmine.ss@gmail.com>
2023-04-11 16:07:58 +0100

    added report_length file and rspec file

---
+++ spec/report_length_spec.rb
@@ -0,0 +1,8 @@
+require 'report_length'
+
+RSpec.describe "report_length method" do
+    it "returns how long the string entered is" do
+        result = report_length("Hello World!")
+        expect(result).to eq "This string was 12 characters long."
+    end
+end

37013cd6944175c0110dd60bde1e32915874c44b
Sharmine <sharmine.ss@gmail.com>
2023-04-11 16:03:25 +0100

    added 2 more test cases for check_codeword

--- spec/check_codeword_spec.rb
+++ spec/check_codeword_spec.rb
@@ -1,8 +1,18 @@
 require 'check_codeword'
 
 RSpec.describe "check_codeword method" do
-    it "checks whether the enter codeword is wrong, close, or correct" do
+    it "checks whether the enter codeword is correct" do
         result = check_codeword("horse")
         expect(result).to eq "Correct! Come in."
+    end
+
+    it "checks whether the enter codeword is close" do
+        result = check_codeword("hope")
+        expect(result).to eq "Close, but nope."
+    end
+
+    it "checks whether the enter codeword is wrong" do
+        result = check_codeword("password")
+        expect(result).to eq "WRONG!"
     end
 end

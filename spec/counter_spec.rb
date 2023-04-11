1ececad2548c0adfb8e118d17200aab82d661919
Sharmine <sharmine.ss@gmail.com>
2023-04-11 16:43:55 +0100

    added second test case to counter

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -1,10 +1,18 @@
 require 'counter'
 
 RSpec.describe Counter do
-  it "counts running total" do
+  it "counts running total when adding a number" do
     counter = Counter.new
     counter.add(5)
     result = counter.report
     expect(result).to eq "Counted to 5 so far."
+  end
+
+  it "counts running total when adding several numbers" do
+    counter = Counter.new
+    counter.add(3)
+    counter.add(6)
+    result = counter.report
+    expect(result).to eq "Counted to 9 so far."
   end
 end

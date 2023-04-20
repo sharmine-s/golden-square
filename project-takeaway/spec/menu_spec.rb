a17d6b686350433528ca3b2d42394e72db639a99
sharmine-s <sharmine.ss@gmail.com>
2023-04-20 21:05:54 +0100

    green test case for get_dish_if_in_menu correctly returning dish object

--- project-takeaway/spec/menu_spec.rb
+++ project-takeaway/spec/menu_spec.rb
@@ -14,4 +14,13 @@ RSpec.describe Menu do
       expect { menu.add_to_menu("edamame") }.to raise_error "Dish should be an object from the Dish class"
     end
   end
+
+  context "Get dish if it's in the menu" do
+    it "Returns the dish object if the dish name exists in the menu" do
+      edamame = double(:Dish, get_name: "edamame", get_price: 2, is_a?: true)
+      menu = Menu.new
+      menu.add_to_menu(edamame)
+      expect(menu.get_dish_if_in_menu("edamame")).to eq edamame
+    end
+  end
 end

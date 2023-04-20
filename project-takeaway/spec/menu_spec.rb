d24cecffbbd3e71d39abe8d02a9baab960ca7a57
sharmine-s <sharmine.ss@gmail.com>
2023-04-20 20:57:18 +0100

    green unit tests for Menu add_to_menu method

--- project-takeaway/spec/menu_spec.rb
+++ project-takeaway/spec/menu_spec.rb
@@ -3,7 +3,7 @@ require 'menu'
 RSpec.describe Menu do
   context "Adds dishes to the menu and shows the menu" do
     it "Creates 1 dish, adds it, and shows it" do
-      edamame = double(:Dish, get_name: "edamame", get_price: 2)
+      edamame = double(:Dish, get_name: "edamame", get_price: 2, is_a?: true)
       menu = Menu.new
       menu.add_to_menu(edamame)
       expect { menu.show_menu }.to output("edamame: £2\n").to_stdout

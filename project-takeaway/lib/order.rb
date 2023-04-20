8ec97d9bd5d75609007e019c4e582c4877fa5cda
sharmine-s <sharmine.ss@gmail.com>
2023-04-20 16:22:02 +0100

    started writing unit tests for Menu class, one green one red

--- project-takeaway/lib/order.rb
+++ project-takeaway/lib/order.rb
@@ -1,3 +1,5 @@
+require 'dish'
+
 class Order
   def initialize
     @order = []
---
+++ project-takeaway/spec/menu_spec.rb
@@ -0,0 +1,17 @@
+require 'menu'
+
+RSpec.describe Menu do
+  context "Adds dishes to the menu and shows the menu" do
+    it "Creates 1 dish, adds it, and shows it" do
+      edamame = double(:Dish, get_name: "edamame", get_price: 2)
+      menu = Menu.new
+      menu.add_to_menu(edamame)
+      expect { menu.show_menu }.to output("edamame: £2\n").to_stdout
+    end
+
+    it "Fails if trying to add something that isn't a Dish object" do
+      menu = Menu.new
+      expect { menu.add_to_menu("edamame") }.to raise_error "Dish should be an object from the Dish class"
+    end
+  end
+end
